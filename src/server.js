45e82b5b05bca5aed41ec116697158f8393e7c4b
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-21 14:18:49 -0300

    teste post

--- src/server.js
+++ src/server.js
@@ -1,20 +1,34 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.json({
-        message: 'Controle de pessoas',
-        nome: 'Elias Fernandes',
-        idade: 29,
-        numFilhos: 0,
-        cidade: 'Curitiba',
-        estado: 'Paraná',
-        pais: 'Brasil'
-    })
-})
+const pessoas = {
+  pessoa1: {
+    nome: "Elias Fernandes",
+    idade: 29,
+    numFilhos: 0,
+    cidade: "Curitiba",
+    estado: "Paraná",
+    pais: "Brasil",
+  },
+  pessoa2: {
+    nome: "Jhess Mattos",
+    idade: 24,
+    numFilhos: 1,
+    cidade: "Niterói",
+    estado: "Rio de Janeiro",
+    pais: "Brasil",
+  },
+};
 
-app.listen(port, ()=>{
-    console.log('🔸Servidor Iniciado🔸')
-})
+app.get("/", (req, res) => {
+  return res.send(pessoas.pessoa1);
+});
 
+app.post("/teste-post", (req, res) => {
+  return res.send(pessoas.pessoa2);
+});
+
+app.listen(port, () => {
+  console.log("🔸Servidor Iniciado🔸");
+});
